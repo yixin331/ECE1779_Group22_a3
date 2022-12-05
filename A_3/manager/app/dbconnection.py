d1de94560b261d4f93ab4caa2bbfb315a2cf1457
yixin331 <yixin331@users.noreply.github.com>
2022-12-04 19:16:13 -0500

    change to dynamodb & change aws

--- A_3/manager/app/dbconnection.py
+++ A_3/manager/app/dbconnection.py
@@ -1,100 +1,563 @@
-import mysql.connector
-from app.config import db_config
+import boto3
+from app.config import aws_config
+from app import dbconnection
+from boto3.dynamodb.conditions import Key
 from datetime import datetime
-from flask import g
 
 
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = connect_to_database()
-    return db
+def initializeDB():
+    db = boto3.client(
+        'dynamodb',
+        aws_config['region'],
+        aws_access_key_id=aws_config['access_key_id'],
+        aws_secret_access_key=aws_config['secret_access_key']
+    )
 
+    try:
+        db.delete_table(Table_name='image')
+    except db.exceptions.ResourceNotFoundException as err:
+        pass
 
-def connect_to_database():
-    return mysql.connector.connect(user=db_config['user'],
-                                   password=db_config['password'],
-                                   host=db_config['host'],
-                                   database=db_config['database'])
+    db.get_waiter('table_not_exists').wait(TableName='image')
+
+    try:
+        table = dynamodb.create_table(
+            TableName='image',
+            KeySchema=[
+                {
+                    'AttributeName': 'ID',
+                    'KeyType': 'HASH'
+                }
+            ],
+            GlobalSecondaryIndexes=[
+                {
+                    'IndexName': "GetImageIndex",
+                    'KeySchema': [
+                        {
+                            'KeyType': 'HASH',
+                            'AttributeName': 'ID'
+                        },
+                        {
+                            'KeyType': 'RANGE',
+                            'AttributeName': 'path'
+                        }
+                    ],
+                    'Projection': {
+                        'ProjectionType': 'KEYS_ONLY'
+                    },
+                    'ProvisionedThroughput': {
+                        'ReadCapacityUnits': 2,
+                        'WriteCapacityUnits': 2
+                    }
+                },
+                {
+                    'IndexName': "ListKeysIndex",
+                    'KeySchema': [
+                        {
+                            'KeyType': 'HASH',
+                            'AttributeName': 'ID'
+                        }
+                    ],
+                    'Projection': {
+                        'ProjectionType': 'KEYS_ONLY'
+                    },
+                    'ProvisionedThroughput': {
+                        'ReadCapacityUnits': 2,
+                        'WriteCapacityUnits': 2
+                    }
+                },
+                {
+                    'IndexName': "SortIDIndex",
+                    'KeySchema': [
+                        {
+                            'KeyType': 'HASH',
+                            'AttributeName': 'ID'
+                        },
+                        {
+                            'KeyType': 'RANGE',
+                            'AttributeName': 'last_edited_time'
+                        }
+                    ],
+                    'Projection': {
+                        'ProjectionType': 'KEYS_ONLY'
+                    },
+                    'ProvisionedThroughput': {
+                        'ReadCapacityUnits': 2,
+                        'WriteCapacityUnits': 2
+                    }
+                },
+                {
+                    'IndexName': "ListLocationsIndex",
+                    'KeySchema': [
+                        {
+                            'KeyType': 'HASH',
+                            'AttributeName': 'location'
+                        }
+                    ],
+                    'Projection': {
+                        'ProjectionType': 'KEYS_ONLY'
+                    },
+                    'ProvisionedThroughput': {
+                        'ReadCapacityUnits': 2,
+                        'WriteCapacityUnits': 2
+                    }
+                },
+                {
+                    'IndexName': "ListTagsIndex",
+                    'KeySchema': [
+                        {
+                            'KeyType': 'HASH',
+                            'AttributeName': 'tag'
+                        }
+                    ],
+                    'Projection': {
+                        'ProjectionType': 'KEYS_ONLY'
+                    },
+                    'ProvisionedThroughput': {
+                        'ReadCapacityUnits': 2,
+                        'WriteCapacityUnits': 2
+                    }
+                },
+                {
+                    'IndexName': "IDEXISTSIndex",
+                    'KeySchema': [
+                        {
+                            'KeyType': 'HASH',
+                            'AttributeName': 'ID'
+                        }
+                    ],
+                    'Projection': {
+                        'ProjectionType': 'ALL'
+                    },
+                    'ProvisionedThroughput': {
+                        'ReadCapacityUnits': 2,
+                        'WriteCapacityUnits': 2
+                    }
+                }
+            ],
+
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'ID',
+                    'AttributeType': 'S'
+                },
+                {
+                    'AttributeName': 'path',
+                    'AttributeType': 'S'
+                },
+                {
+                    'AttributeName': 'location',
+                    'AttributeType': 'S'
+                },
+                {
+                    'AttributeName': 'tag',
+                    'AttributeType': 'S'
+                },
+                {
+                    'AttributeName': 'last_edited_time',
+                    'AttributeType': 'S'
+                }
+            ],
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 10,
+                'WriteCapacityUnits': 10
+            }
+        )
+    except db.exceptions.ResourceInUseException as error:
+        pass
+
+    db.get_waiter('table_exists').wait(TableName='image')
+
+    try:
+        db.delete_table(Table_name='memcache_config')
+    except db.exceptions.ResourceNotFoundException as err:
+        pass
+
+    db.get_waiter('table_not_exists').wait(TableName='memcache_config')
+
+    try:
+        table = dynamodb.create_table(
+            TableName='memcache_config',
+            KeySchema=[
+                {
+                    'AttributeName': 'updated_time',
+                    'KeyType': 'RANGE'
+                }
+            ],
+            GlobalSecondaryIndexes=[
+                {
+                    'IndexName': "GetConfigIndex",
+                    'KeySchema': [
+                        {
+                            'KeyType': 'RANGE',
+                            'AttributeName': 'updated_time'
+                        }
+                    ],
+                    'Projection': {
+                        'ProjectionType': 'ALL'
+                    },
+                    'ProvisionedThroughput': {
+                        'ReadCapacityUnits': 2,
+                        'WriteCapacityUnits': 2
+                    }
+                }
+            ],
+
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'updated_time',
+                    'AttributeType': 'S'
+                },
+                {
+                    'AttributeName': 'capacity',
+                    'AttributeType': 'N'
+                },
+                {
+                    'AttributeName': 'policy',
+                    'AttributeType': 'S'
+                }
+            ],
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 10,
+                'WriteCapacityUnits': 10
+            }
+        )
+    except db.exceptions.ResourceInUseException as error:
+        pass
+
+    db.get_waiter('table_exists').wait(TableName='memcache_config')
+
+    put_config(128, 'LRU')
+
+    try:
+        db.delete_table(Table_name='memcache_mode')
+    except db.exceptions.ResourceNotFoundException as err:
+        pass
+
+    db.get_waiter('table_not_exists').wait(TableName='memcache_mode')
+
+    try:
+        table = dynamodb.create_table(
+            TableName='memcache_mode',
+            KeySchema=[
+                {
+                    'AttributeName': 'updated_time',
+                    'KeyType': 'RANGE'
+                }
+            ],
+            GlobalSecondaryIndexes=[
+                {
+                    'IndexName': "GetModeIndex",
+                    'KeySchema': [
+                        {
+                            'KeyType': 'RANGE',
+                            'AttributeName': 'updated_time'
+                        }
+                    ],
+                    'Projection': {
+                        'ProjectionType': 'ALL'
+                    },
+                    'ProvisionedThroughput': {
+                        'ReadCapacityUnits': 2,
+                        'WriteCapacityUnits': 2
+                    }
+                }
+            ],
+
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'updated_time',
+                    'AttributeType': 'S'
+                },
+                {
+                    'AttributeName': 'num_node',
+                    'AttributeType': 'N'
+                },
+                {
+                    'AttributeName': 'mode',
+                    'AttributeType': 'S'
+                },
+                {
+                    'AttributeName': 'max_thr',
+                    'AttributeType': 'N'
+                },
+                {
+                    'AttributeName': 'min_thr',
+                    'AttributeType': 'N'
+                },
+                {
+                    'AttributeName': 'expand_ratio',
+                    'AttributeType': 'N'
+                },
+                {
+                    'AttributeName': 'shrink_ratio',
+                    'AttributeType': 'N'
+                },
+            ],
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 10,
+                'WriteCapacityUnits': 10
+            }
+        )
+    except db.exceptions.ResourceInUseException as error:
+        pass
+
+    db.get_waiter('table_exists').wait(TableName='memcache_mode')
+
+    put_mode(1, 'Manual', 1, 0, 1, 1)
 
 
 def get_image(key):
-    cnx = get_db()
+    db = boto3.client(
+        'dynamodb',
+        aws_config['region'],
+        aws_access_key_id=aws_config['access_key_id'],
+        aws_secret_access_key=aws_config['secret_access_key']
+    )
 
-    cursor = cnx.cursor()
-    query = "SELECT path FROM image WHERE ID = %s;"
-    cursor.execute(query, (key,))
+    table = db.Table(tableName='image')
 
-    return cursor
+    response = table.query(
+        IndexName='GetImageIndex',
+        KeyConditionExpression=Key('ID').eq(key),
+        ProjectionExpression='path'
+    )
+
+    if len(response['Items']) == 0:
+        return ""
+
+    return response['Items'][0]['path']
 
 
-def put_image(key, path):
-    cnx = get_db()
+def put_image(key, path, label, city):
+    db = boto3.client(
+        'dynamodb',
+        aws_config['region'],
+        aws_access_key_id=aws_config['access_key_id'],
+        aws_secret_access_key=aws_config['secret_access_key']
+    )
 
-    cursor = cnx.cursor()
-    query = "INSERT INTO image (ID, path, last_edited_time) VALUES (%s, %s, %s) ON DUPLICATE KEY UPDATE path = %s, last_edited_time = %s;"
-    cursor.execute(query, (key, path, datetime.now(), path, datetime.now()))
+    table = db.Table(tableName='image')
 
-    cnx.commit()
+    # invalidate key if key exists
+    if key_exists(key) == {}:
+        response = table.put_item(
+            Item={
+                'ID': key,
+                'path': path,
+                'location': city,
+                'tag': label,
+                'last_edited_time': datetime.now().isoformat()
+            }
+        )
+    else:
+        response = table.update_item(
+            Key={
+                'ID': key
+            },
+            UpdateExpression="set path=:p, location=:c, tag=:l, last_edited_time =:t",
+            ExpressionAttributeValues={
+                ':p': path,
+                ':c': city,
+                ':l': label,
+                ':t': datetime.now().isoformat()
+            }
+        )
 
 
 def list_keys():
-    cnx = get_db()
+    db = boto3.client(
+        'dynamodb',
+        aws_config['region'],
+        aws_access_key_id=aws_config['access_key_id'],
+        aws_secret_access_key=aws_config['secret_access_key']
+    )
 
-    cursor = cnx.cursor()
-    query = "SELECT ID FROM image;"
-    cursor.execute(query)
+    table = db.Table(tableName='image')
 
-    return cursor
+    response = table.query(
+        IndexName='ListKeysIndex'
+    )
+
+    records = []
+
+    for i in response['Items']:
+        if i['ID'] not in records:
+            records.append(i['ID'])
+
+    return records
+
+
+def key_exists(key):
+    db = boto3.client(
+        'dynamodb',
+        aws_config['region'],
+        aws_access_key_id=aws_config['access_key_id'],
+        aws_secret_access_key=aws_config['secret_access_key']
+    )
+
+    table = db.Table(tableName='image')
+
+    response = table.query(
+        IndexName='IDEXISTSIndex',
+        KeyConditionExpression=Key('ID').eq(key)
+    )
+
+    if len(response['Items']) == 0:
+        return {}
+
+    return response['Items'][0]
 
 
 def put_config(capacity, policy):
-    cnx = get_db()
+    db = boto3.client(
+        'dynamodb',
+        aws_config['region'],
+        aws_access_key_id=aws_config['access_key_id'],
+        aws_secret_access_key=aws_config['secret_access_key']
+    )
 
-    cursor = cnx.cursor()
-    query = "INSERT INTO memcache_config (updated_time, capacity, policy) VALUES (%s, %s, %s);"
-    cursor.execute(query, (datetime.now(), capacity, policy))
+    table = db.Table(tableName='memcache_config')
 
-    cnx.commit()
+    response = table.put_item(
+        Item={
+            'updated_time': datetime.now().isoformat(),
+            'capacity': capacity,
+            'policy': policy
+        }
+    )
 
 
 def put_mode(num_node, mode, max_thr, min_thr, expand_ratio, shrink_ratio):
-    cnx = get_db()
+    db = boto3.client(
+        'dynamodb',
+        aws_config['region'],
+        aws_access_key_id=aws_config['access_key_id'],
+        aws_secret_access_key=aws_config['secret_access_key']
+    )
 
-    cursor = cnx.cursor()
-    query = "INSERT INTO memcache_mode (updated_time, num_node, mode, max_thr, min_thr, expand_ratio, shrink_ratio) VALUES (%s, %s, %s, %s, %s, %s, %s);"
-    cursor.execute(query, (datetime.now(), num_node, mode, max_thr, min_thr, expand_ratio, shrink_ratio))
+    table = db.Table(tableName='memcache_mode')
 
-    cnx.commit()
+    response = table.put_item(
+        Item={
+            'updated_time': datetime.now().isoformat(),
+            'num_node': num_node,
+            'mode': mode,
+            'max_thr': max_thr,
+            'min_thr': min_thr,
+            'expand_ratio': expand_ratio,
+            'shrink_ratio': shrink_ratio
+        }
+    )
 
 
 def clear():
-    cnx = get_db()
+    db = boto3.client(
+        'dynamodb',
+        aws_config['region'],
+        aws_access_key_id=aws_config['access_key_id'],
+        aws_secret_access_key=aws_config['secret_access_key']
+    )
 
-    cursor = cnx.cursor()
-    query = "DELETE FROM image;"
-    cursor.execute(query)
+    table = db.Table(tableName='image')
 
-    cnx.commit()
+    scan = table.scan()
+    with table.batch_writer() as batch:
+        for each in scan['Items']:
+            batch.delete_item(
+                Key={
+                    'ID': each['ID']
+                }
+            )
 
 
 def update_image(key):
-    cnx = get_db()
+    db = boto3.client(
+        'dynamodb',
+        aws_config['region'],
+        aws_access_key_id=aws_config['access_key_id'],
+        aws_secret_access_key=aws_config['secret_access_key']
+    )
 
-    cursor = cnx.cursor()
-    query = "UPDATE image SET last_edited_time = %s WHERE ID = %s;"
-    cursor.execute(query, (datetime.now(), key))
-
-    cnx.commit()
+    table = db.Table(tableName='image')
+    response = table.update_item(
+        Key={
+            'ID': key
+        },
+        UpdateExpression="set last_edited_time =:t",
+        ExpressionAttributeValues={
+            ':t': datetime.now().isoformat()
+        }
+    )
 
 
 def sort_by_time(key_list):
-    cnx = get_db()
+    db = boto3.client(
+        'dynamodb',
+        aws_config['region'],
+        aws_access_key_id=aws_config['access_key_id'],
+        aws_secret_access_key=aws_config['secret_access_key']
+    )
 
-    ids_to_sort = ', '.join(str(id) for id in key_list)
+    table = db.Table(tableName='image')
 
-    cursor = cnx.cursor()
-    query = "SELECT ID FROM image WHERE ID IN ({ids}) ORDER BY last_edited_time;"
-    cursor.execute(query.format(ids=ids_to_sort))
+    # ids_to_sort = ', '.join(str(id) for id in key_list)
 
-    return cursor
+    response = table.query(
+        IndexName='SortIDIndex',
+        FilterExpression=Attr('ID').is_in(key_list),
+        ScanIndexForward=true
+    )
+    records = []
+
+    for i in response['Items']:
+        if i['ID'] not in records:
+            records.append(i['ID'])
+
+    return records
+
+
+def list_locations():
+    db = boto3.client(
+        'dynamodb',
+        aws_config['region'],
+        aws_access_key_id=aws_config['access_key_id'],
+        aws_secret_access_key=aws_config['secret_access_key']
+    )
+
+    table = db.Table(tableName='image')
+
+    response = table.query(
+        IndexName='ListLocationsIndex'
+    )
+
+    records = []
+
+    for i in response['Items']:
+        if i['location'] != '' and i['location'] not in records:
+            records.append(i['location'])
+
+    return records
+
+
+def list_tags():
+    db = boto3.client(
+        'dynamodb',
+        aws_config['region'],
+        aws_access_key_id=aws_config['access_key_id'],
+        aws_secret_access_key=aws_config['secret_access_key']
+    )
+
+    table = db.Table(tableName='image')
+
+    response = table.query(
+        IndexName='ListTagsIndex'
+    )
+
+    records = []
+
+    for i in response['Items']:
+        if i['tag'] != '' and i['tag'] not in records:
+            records.append(i['tag'])
+
+    return records
